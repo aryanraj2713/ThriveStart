00091d8ab205ae06632efd04ecb60823829599f7
Jatin <main.jatink@gmail.com>
2024-04-08 16:37:00 +0530

    feat(ui): use cards for dashboard

--- client/src/components/nav.tsx
+++ client/src/components/nav.tsx
@@ -32,7 +32,7 @@ export const Nav = () => {
     <header className="fixed z-50 top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6 justify-between w-full">
       <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
         <Link
-          href=""
+          href="/"
           className="flex items-center gap-2 text-lg font-semibold md:text-base"
         >
           <Package2 className="h-6 w-6" />
